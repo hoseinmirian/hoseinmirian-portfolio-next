bce6a3630c03365c409e52c6658d8eb933e21848
hoseinmirian <hosein.mirian@gmail.com>
2025-10-07 07:38:16 +0100

    fix header link

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,5 +1,6 @@
 import NavLinks from '@/components/NavLinks'
 import { ThemeToggle } from '@/components/ThemeToggle'
+import Link from 'next/link'
 
 const MAIN_NAV_LINKS = [
   { label: 'Home', href: '/' },
@@ -11,7 +12,12 @@ const MAIN_NAV_LINKS = [
 export default function Navigation() {
   return (
     <nav className='mx-auto flex max-w-7xl items-center justify-between'>
-      <p className='text-2xl font-bold'>Hossein Mirian</p>
+      <Link
+        className='text-2xl font-bold'
+        href='/'
+      >
+        Hossein Mirian
+      </Link>
       <NavLinks navLinks={MAIN_NAV_LINKS} />
       <ThemeToggle />
     </nav>
