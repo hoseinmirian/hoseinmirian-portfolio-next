c0f3e14e909afe5892eff0c1f9113337aa137cc1
hoseinmirian <hosein.mirian@gmail.com>
2025-10-07 07:39:48 +0100

    updated manifist

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,9 +2,9 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next boilerplate hossein',
-    short_name: 'Next boilerplate hossein',
-    description: 'Next boilerplate hossein ready to use',
+    name: 'Hossein Mirian portfolio',
+    short_name: 'hossein mirian',
+    description: 'Hossein Mirian personal portfolio website',
     start_url: '/',
     display: 'standalone',
     background_color: '#fff',
